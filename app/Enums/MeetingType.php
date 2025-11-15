bc67244fe79cc942df5b1620eadd0ff961bc7ce4
oddegen <nahomtensae@gmail.com>
2025-11-15 09:53:29 +0300

    fix: enum name

--- app/Enums/MeetingType.php
+++ app/Enums/MeetingType.php
@@ -6,6 +6,6 @@ enum MeetingType: string
 {
     use HasEnumValues;
     case OnlineCall = 'online_call';
-    case InPersonMeeting = 'in-person meeting';
+    case InPersonMeeting = 'in_person_meeting';
 
 }
